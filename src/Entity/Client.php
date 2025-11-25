ab632d8a2a4072724a6188fc9153103c731d1e92
Marc-AntoineMarie <mmarie.galaxie@gmail.com>
2025-11-25 18:51:35 +0100

    adding filter, order, validation for client to

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -7,9 +7,22 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use Symfony\Component\Validator\Constraints as Assert;
 
-#[ORM\Entity(repositoryClass: ClientRepository::class)]#[ApiResource]
-
+#[ORM\Entity(repositoryClass: ClientRepository::class)]
+#[ApiResource(
+    paginationEnabled: true,
+    paginationItemsPerPage: 10
+)]
+#[ApiFilter(OrderFilter::class, properties: ['name', 'email'], arguments: ['orderParameterName' => 'order'])]
+#[ApiFilter(SearchFilter::class, properties: [
+    'name' => 'partial',
+    'email' => 'partial',
+    'phone' => 'exact'
+])]
 class Client
 {
     #[ORM\Id]
@@ -18,18 +31,25 @@ class Client
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Le nom est obligatoire")]
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "L'email est obligatoire")]
+    #[Assert\Email(message: "L'email '{{ value }}' n'est pas valide.")]
     private ?string $email = null;
 
     #[ORM\Column(length: 20, nullable: true)]
+    #[Assert\Length(
+        max: 20,
+        maxMessage: "Le numéro de téléphone ne peut pas dépasser {{ limit }} caractères."
+    )]
     private ?string $phone = null;
 
     /**
      * @var Collection<int, Contact>
      */
-    #[ORM\OneToMany(targetEntity: Contact::class, mappedBy: 'client')]
+    #[ORM\OneToMany(targetEntity: Contact::class, mappedBy: 'client', cascade: ['persist', 'remove'])]
     private Collection $contacts;
 
     public function __construct()
@@ -50,7 +70,6 @@ class Client
     public function setName(string $name): static
     {
         $this->name = $name;
-
         return $this;
     }
 
@@ -62,7 +81,6 @@ class Client
     public function setEmail(string $email): static
     {
         $this->email = $email;
-
         return $this;
     }
 
@@ -74,7 +92,6 @@ class Client
     public function setPhone(?string $phone): static
     {
         $this->phone = $phone;
-
         return $this;
     }
 
@@ -99,7 +116,6 @@ class Client
     public function removeContact(Contact $contact): static
     {
         if ($this->contacts->removeElement($contact)) {
-            // set the owning side to null (unless already changed)
             if ($contact->getClient() === $this) {
                 $contact->setClient(null);
             }
